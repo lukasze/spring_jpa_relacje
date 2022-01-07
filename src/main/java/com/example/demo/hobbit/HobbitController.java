3fa9840fae1a2f9f3f611dd23ede9317c4f86e86
lukasze <wyspianski.lukasz@gmail.com>
2022-01-07 17:53:17 +0100

    cRud controller <-> service <-> repository

--- src/main/java/com/example/demo/hobbit/HobbitController.java
+++ src/main/java/com/example/demo/hobbit/HobbitController.java
@@ -1,4 +1,19 @@
 package com.example.demo.hobbit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class HobbitController {
+
+    @Autowired
+    private HobbitService hobbitService;
+
+    @GetMapping("/hobbits")
+    List<Hobbit> findAll() {
+        return hobbitService.findAll();
+    }
 }
--- src/main/java/com/example/demo/hobbit/HobbitService.java
+++ src/main/java/com/example/demo/hobbit/HobbitService.java
@@ -1,4 +1,17 @@
 package com.example.demo.hobbit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class HobbitService {
+
+    @Autowired
+    private HobbitRepository hobbitRepository;
+
+    public List<Hobbit> findAll() {
+        return hobbitRepository.findAll();
+    }
 }
